617c91374ca5f1a6cc21c3aa3813eb621c37ad9e
zuhrahmohammed <zuhrahmohammed04@gmail.com>
2025-04-18 17:04:27 -0400

    fixing

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ import os
 from flask import Flask, render_template, request
 from similarity_model import SimilarityModel
 
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)  
+
 app = Flask(__name__, template_folder='templates')
 model: SimilarityModel = SimilarityModel("ontology_descriptions.json")
 
@@ -17,6 +22,3 @@ def check():
     return render_template("index.html", score=round(score, 4), message=message)
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)  
