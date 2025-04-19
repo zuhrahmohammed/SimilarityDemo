101e58b942b8278f186c532ffd01e11802bbf9e5
zuhrahmohammed <zuhrahmohammed04@gmail.com>
2025-04-18 21:02:28 -0400

    fix

--- app.py
+++ app.py
@@ -16,6 +16,6 @@ def check():
     score = model.compare_text(message)
     return render_template("index.html", score=round(score, 4), message=message)
 
-#if __name__ == "__main__":
-#    port = int(os.environ.get("PORT", 5000))
-#    app.run(host="0.0.0.0", port=port, debug=False)  
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)  
